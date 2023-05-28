c7cd397afc1450660cac3e8483ebbc06d9290fad
neeeraj52 <132575684+neeeraj52@users.noreply.github.com>
2023-05-28 12:15:59 +0000

    CHNGED

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine,text
-my_secret="mysql+pymysql://obzelc11m8fizde85sua:pscale_pw_iYRsbD3IbCQZf3E1ipvvufAd7Rd4T4YUJG3uLjXlkqq@aws.connect.psdb.cloud/vignanlms?charset=utf8mb4"
+import OS
+my_secret=os.environ['DB_STRING']
 engine = create_engine(my_secret,connect_args={
   "ssl":{
     "ssl_ca":"/etc/ssl/cert.pem"
