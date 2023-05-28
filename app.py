78deef8891db37ff3caee6e91e98c60cfdfbc4e7
neeeraj52 <132575684+neeeraj52@users.noreply.github.com>
2023-05-28 06:30:11 +0000

    made some changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
@@ -11,6 +11,7 @@ def hello():
 @app.route('/faculty')
 def faculty():
   return render_template("faculty.html")
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
