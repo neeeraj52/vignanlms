f07d90b8d5e4376667c78c7771b8aa354a39a498
neeeraj52 <132575684+neeeraj52@users.noreply.github.com>
2023-05-29 06:36:54 +0000

    added few other templates

--- app.py
+++ app.py
@@ -8,5 +8,20 @@ def home():
   return render_template("loginpage.html")
 
 
+@app.route("/faculty")
+def faculty():
+  return render_template("faculty.html")
+
+
+@app.route("/attendence")
+def attendence():
+  return render_template("attendence.html")
+
+
+@app.route("/assignments")
+def assignments():
+  return render_template("assignmnts.html")
+
+
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
