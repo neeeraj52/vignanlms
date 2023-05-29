ea6daf9a2e52bee422084e2ac6710769f6d22450
neeeraj52 <132575684+neeeraj52@users.noreply.github.com>
2023-05-29 04:31:34 +0000

    added login page

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-if __name__=="__main__":
-  app.run(host="0.0.0.0",debug=True)
+def home():
+  return render_template("loginpage.html")
+
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", debug=True)
