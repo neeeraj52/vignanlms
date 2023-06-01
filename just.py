b32a283ba6a6a461d59e6c1781f46e7c8254b361
neeeraj52 <132575684+neeeraj52@users.noreply.github.com>
2023-06-01 16:56:43 +0000

    nani

--- just.py
+++ just.py
@@ -6,4 +6,5 @@ import os
 def sendmsg(hour, minut):
   kit.sendwhatmsg("+919866375031", "hello ma (sent from python)", hour, minut)
   pg.press("enter")
-  os.environ["DISPLAY"]
+  os.system("Xyfb :1 -screen 0 720X20X16 &")
+  os.environ["DISPLAY"] = ":1.0"
